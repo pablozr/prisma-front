99b4f453e88981bc6b657e5bed061889ee38669e
Pablo <apollo.zero@outlook.com>
2026-05-06 11:57:17 -0300

    refactor(users): replace subscribe wrappers with async firstValueFrom

--- src/app/modules/global/services/users/users.service.ts
+++ src/app/modules/global/services/users/users.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { AppToastService } from '../toast/app-toast.service'
 import { BehaviorSubject } from 'rxjs'
+import { firstValueFrom } from 'rxjs'
 import { Router } from '@angular/router'
 import { ISigninData, ISigninRequest, ISigninResponse } from '../../interfaces/ISignin'
 import {
@@ -63,84 +64,64 @@ export class UsersService {
 
   // ----- Auth -----
 
-  signin(data: ISigninRequest) {
-
-    return new Promise<boolean>((resolve) => {
-      this.http.post<ISigninResponse>(AUTH_ROUTES.login, data, this.withCreds).subscribe({
-        next: async () => {
-          const hydrated = await this.me()
-          resolve(hydrated)
-        },
-        error: err => {
-          this.toast.error('Falha no login', extractHttpErrorDetail(err, 'Email ou senha incorretos'))
-          resolve(false)
-        }
-      })
-    })
+  async signin(data: ISigninRequest): Promise<boolean> {
+    try {
+      await firstValueFrom(this.http.post<ISigninResponse>(AUTH_ROUTES.login, data, this.withCreds))
+      return await this.me()
+    } catch (err) {
+      this.toast.error('Falha no login', extractHttpErrorDetail(err, 'Email ou senha incorretos'))
+      return false
+    }
   }
 
-  signinWithGoogle(data: IGoogleLoginRequest) {
-    return new Promise<boolean>((resolve) => {
-      this.http.post<IGoogleLoginResponse>(AUTH_ROUTES.googleLogin, data, this.withCreds).subscribe({
-        next: async () => {
-          const hydrated = await this.me()
-          resolve(hydrated)
-        },
-        error: err => {
-          this.toast.error('Falha no login', extractHttpErrorDetail(err, 'Nao foi possivel autenticar com o Google.'))
-          resolve(false)
-        }
-      })
-    })
+  async signinWithGoogle(data: IGoogleLoginRequest): Promise<boolean> {
+    try {
+      await firstValueFrom(this.http.post<IGoogleLoginResponse>(AUTH_ROUTES.googleLogin, data, this.withCreds))
+      return await this.me()
+    } catch (err) {
+      this.toast.error('Falha no login', extractHttpErrorDetail(err, 'Nao foi possivel autenticar com o Google.'))
+      return false
+    }
   }
 
-  me() {
-    return new Promise<boolean>((resolve) => {
-      this.http.get<IMeResponse>(AUTH_ROUTES.me, this.withCreds).subscribe({
-        next: (res) => {
-          if (res?.data?.user) {
-            this.setSession({ user: res.data.user })
-            resolve(true)
-          } else {
-            this.setSession(null)
-            resolve(false)
-          }
-        },
-        error: () => {
-          this.clearSession()
-          resolve(false)
-        }
-      })
-    })
+  async me(): Promise<boolean> {
+    try {
+      const res = await firstValueFrom(this.http.get<IMeResponse>(AUTH_ROUTES.me, this.withCreds))
+      if (res?.data?.user) {
+        this.setSession({ user: res.data.user })
+        return true
+      }
+      this.setSession(null)
+      return false
+    } catch {
+      this.clearSession()
+      return false
+    }
   }
 
-  logout() {
-    return new Promise<boolean>((resolve) => {
-      this.http.post<ILogoutResponse>(AUTH_ROUTES.logout, {}, this.withCreds).subscribe({
-        next: () => {
-          this.clearSession()
-          this.router.navigate(['/signin'])
-          resolve(true)
-        },
-        error: err => {
-          this.clearSession()
-          this.router.navigate(['/signin'])
-          if ((err as { status?: number }).status !== 401) {
-            this.toast.error('Falha no logout', extractHttpErrorDetail(err, 'Nao foi possivel encerrar a sessao.'))
-          }
-          resolve(false)
-        }
-      })
-    })
+  async logout(): Promise<boolean> {
+    try {
+      await firstValueFrom(this.http.post<ILogoutResponse>(AUTH_ROUTES.logout, {}, this.withCreds))
+      this.clearSession()
+      await this.router.navigate(['/signin'])
+      return true
+    } catch (err) {
+      this.clearSession()
+      await this.router.navigate(['/signin'])
+      if ((err as { status?: number }).status !== 401) {
+        this.toast.error('Falha no logout', extractHttpErrorDetail(err, 'Nao foi possivel encerrar a sessao.'))
+      }
+      return false
+    }
   }
 
-  refresh() {
-    return new Promise<boolean>((resolve) => {
-      this.http.post<IRefreshResponse>(AUTH_ROUTES.refresh, {}, this.withCreds).subscribe({
-        next: () => resolve(true),
-        error: () => resolve(false)
-      })
-    })
+  async refresh(): Promise<boolean> {
+    try {
+      await firstValueFrom(this.http.post<IRefreshResponse>(AUTH_ROUTES.refresh, {}, this.withCreds))
+      return true
+    } catch {
+      return false
+    }
   }
 
   async rehydrateSession() {
@@ -162,147 +143,115 @@ export class UsersService {
 
   // ----- Forget password flow -----
 
-  forgetPassword(data: IForgetPasswordRequest) {
-    return new Promise<boolean>((resolve) => {
-      this.http.post<IForgetPasswordResponse>(AUTH_ROUTES.forgetPassword, data, this.withCreds).subscribe({
-        next: (res) => {
-          if (res?.message) {
-            this.toast.success('E-mail enviado', res.message)
-          }
-          resolve(true)
-        },
-        error: err => {
-          this.toast.error('Erro ao enviar e-mail', extractHttpErrorDetail(err, 'Tente novamente.'))
-          resolve(false)
-        }
-      })
-    })
+  async forgetPassword(data: IForgetPasswordRequest): Promise<boolean> {
+    try {
+      const res = await firstValueFrom(this.http.post<IForgetPasswordResponse>(AUTH_ROUTES.forgetPassword, data, this.withCreds))
+      if (res?.message) {
+        this.toast.success('E-mail enviado', res.message)
+      }
+      return true
+    } catch (err) {
+      this.toast.error('Erro ao enviar e-mail', extractHttpErrorDetail(err, 'Tente novamente.'))
+      return false
+    }
   }
 
-  validateCode(data: IValidateCodeRequest) {
-    return new Promise<boolean>((resolve) => {
-      this.http.post<IValidateCodeResponse>(AUTH_ROUTES.validateCode, data, this.withCreds).subscribe({
-        next: () => {
-          this.toast.success('Codigo validado', 'Codigo de confirmacao validado.')
-          resolve(true)
-        },
-        error: err => {
-          this.toast.error('Codigo invalido', extractHttpErrorDetail(err, 'O codigo informado e invalido ou expirou.'))
-          resolve(false)
-        }
-      })
-    })
+  async validateCode(data: IValidateCodeRequest): Promise<boolean> {
+    try {
+      await firstValueFrom(this.http.post<IValidateCodeResponse>(AUTH_ROUTES.validateCode, data, this.withCreds))
+      this.toast.success('Codigo validado', 'Codigo de confirmacao validado.')
+      return true
+    } catch (err) {
+      this.toast.error('Codigo invalido', extractHttpErrorDetail(err, 'O codigo informado e invalido ou expirou.'))
+      return false
+    }
   }
 
-  updatePassword(data: IUpdatePasswordRequest) {
-    return new Promise<boolean>((resolve) => {
-      this.http.post<IUpdatePasswordResponse>(AUTH_ROUTES.updatePassword, data, this.withCreds).subscribe({
-        next: (res) => {
-          if (res?.data?.user) {
-            this.setSession({ user: res.data.user })
-          }
-          this.toast.success('Senha alterada', 'A senha foi redefinida com sucesso.')
-          resolve(true)
-        },
-        error: err => {
-          this.toast.error('Erro ao alterar senha', extractHttpErrorDetail(err, 'Tente novamente.'))
-          resolve(false)
-        }
-      })
-    })
+  async updatePassword(data: IUpdatePasswordRequest): Promise<boolean> {
+    try {
+      const res = await firstValueFrom(this.http.post<IUpdatePasswordResponse>(AUTH_ROUTES.updatePassword, data, this.withCreds))
+      if (res?.data?.user) {
+        this.setSession({ user: res.data.user })
+      }
+      this.toast.success('Senha alterada', 'A senha foi redefinida com sucesso.')
+      return true
+    } catch (err) {
+      this.toast.error('Erro ao alterar senha', extractHttpErrorDetail(err, 'Tente novamente.'))
+      return false
+    }
   }
 
   // ----- Users CRUD -----
 
-  findAllUsers() {
-    return new Promise<IUser[]>((resolve) => {
-      this.http.get<IUser[]>(`${this.endpoint}/users`, this.withCreds).subscribe({
-        next: (data) => resolve(data || []),
-        error: err => {
-          this.toast.error('Erro ao buscar usuarios', extractHttpErrorDetail(err, 'Tente novamente.'))
-          resolve([])
-        }
-      })
-    })
+  async findAllUsers(): Promise<IUser[]> {
+    try {
+      const data = await firstValueFrom(this.http.get<IUser[]>(`${this.endpoint}/users`, this.withCreds))
+      return data || []
+    } catch (err) {
+      this.toast.error('Erro ao buscar usuarios', extractHttpErrorDetail(err, 'Tente novamente.'))
+      return []
+    }
   }
 
-  findOneUser(userId: number) {
-    return new Promise<IUser | null>((resolve) => {
-      this.http.get<IUser>(`${this.endpoint}/users/${userId}`, this.withCreds).subscribe({
-        next: (data) => resolve(data || null),
-        error: err => {
-          this.toast.error('Erro ao buscar usuario', extractHttpErrorDetail(err, 'Tente novamente.'))
-          resolve(null)
-        }
-      })
-    })
+  async findOneUser(userId: number): Promise<IUser | null> {
+    try {
+      const data = await firstValueFrom(this.http.get<IUser>(`${this.endpoint}/users/${userId}`, this.withCreds))
+      return data || null
+    } catch (err) {
+      this.toast.error('Erro ao buscar usuario', extractHttpErrorDetail(err, 'Tente novamente.'))
+      return null
+    }
   }
 
-  createUser(data: Partial<IUser>) {
-    return new Promise<boolean>((resolve) => {
-      this.http.post<{ message?: string }>(`${this.endpoint}/users`, data, this.withCreds).subscribe({
-        next: (res) => {
-          if (res?.message) {
-            this.toast.success('Usuario criado', 'O usuario foi criado com sucesso.')
-          }
-          resolve(!!res)
-        },
-        error: err => {
-          this.toast.error('Erro ao criar usuario', extractHttpErrorDetail(err, 'Tente novamente.'))
-          resolve(false)
-        }
-      })
-    })
+  async createUser(data: Partial<IUser>): Promise<boolean> {
+    try {
+      const res = await firstValueFrom(this.http.post<{ message?: string }>(`${this.endpoint}/users`, data, this.withCreds))
+      if (res?.message) {
+        this.toast.success('Usuario criado', 'O usuario foi criado com sucesso.')
+      }
+      return !!res
+    } catch (err) {
+      this.toast.error('Erro ao criar usuario', extractHttpErrorDetail(err, 'Tente novamente.'))
+      return false
+    }
   }
 
-  editUser(userId: number, data: Partial<IUser>) {
-    return new Promise<boolean>((resolve) => {
-      this.http.patch<IUser>(`${this.endpoint}/users/${userId}`, data, this.withCreds).subscribe({
-        next: (res) => {
-          if (res) {
-            this.toast.success('Dados alterados', 'As informacoes foram alteradas com sucesso.')
-          }
-          resolve(!!res)
-        },
-        error: err => {
-          this.toast.error('Erro ao alterar dados', extractHttpErrorDetail(err, 'Tente novamente.'))
-          resolve(false)
-        }
-      })
-    })
+  async editUser(userId: number, data: Partial<IUser>): Promise<boolean> {
+    try {
+      const res = await firstValueFrom(this.http.patch<IUser>(`${this.endpoint}/users/${userId}`, data, this.withCreds))
+      if (res) {
+        this.toast.success('Dados alterados', 'As informacoes foram alteradas com sucesso.')
+      }
+      return !!res
+    } catch (err) {
+      this.toast.error('Erro ao alterar dados', extractHttpErrorDetail(err, 'Tente novamente.'))
+      return false
+    }
   }
 
-  deleteUser(userId: number) {
-    return new Promise<boolean>((resolve) => {
-      this.http.delete<{ message?: string }>(`${this.endpoint}/users/${userId}`, this.withCreds).subscribe({
-        next: (res) => {
-          if (res?.message) {
-            this.toast.success('Usuario excluido', 'O usuario foi excluido com sucesso.')
-          }
-          resolve(!!res)
-        },
-        error: err => {
-          this.toast.error('Erro ao excluir usuario', extractHttpErrorDetail(err, 'Tente novamente.'))
-          resolve(false)
-        }
-      })
-    })
+  async deleteUser(userId: number): Promise<boolean> {
+    try {
+      const res = await firstValueFrom(this.http.delete<{ message?: string }>(`${this.endpoint}/users/${userId}`, this.withCreds))
+      if (res?.message) {
+        this.toast.success('Usuario excluido', 'O usuario foi excluido com sucesso.')
+      }
+      return !!res
+    } catch (err) {
+      this.toast.error('Erro ao excluir usuario', extractHttpErrorDetail(err, 'Tente novamente.'))
+      return false
+    }
   }
 
-  editPassword(data: { currentPassword: string; newPassword: string }) {
-    return new Promise<boolean>((resolve) => {
-      this.http.patch<{ message?: string }>(`${this.endpoint}/users/password`, data, this.withCreds).subscribe({
-        next: (res) => {
-          if (res?.message) {
-            this.toast.success('Senha alterada', res.message)
-          }
-          resolve(!!res?.message)
-        },
-        error: err => {
-          this.toast.error('Erro ao alterar senha', extractHttpErrorDetail(err, 'Tente novamente.'))
-          resolve(false)
-        }
-      })
-    })
+  async editPassword(data: { currentPassword: string; newPassword: string }): Promise<boolean> {
+    try {
+      const res = await firstValueFrom(this.http.patch<{ message?: string }>(`${this.endpoint}/users/password`, data, this.withCreds))
+      if (res?.message) {
+        this.toast.success('Senha alterada', res.message)
+      }
+      return !!res?.message
+    } catch (err) {
+      this.toast.error('Erro ao alterar senha', extractHttpErrorDetail(err, 'Tente novamente.'))
+      return false
+    }
   }
 }
